eaa8d35da5e49359b6dc2f3fbe4c1b20ed00625c
JON_COOLER <jonathan.r.cooler@gmail.com>
2022-06-10 14:47:21 -0400

    Updates

---
+++ precache-manifest.888acc1e7bea2a7d14c127ef48255e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02962e3ff073e08549f5",
+    "url": "/react-todo-list/static/css/main.a18211a8.chunk.css"
+  },
+  {
+    "revision": "02962e3ff073e08549f5",
+    "url": "/react-todo-list/static/js/main.02962e3f.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/react-todo-list/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "035635a073ea96040c86",
+    "url": "/react-todo-list/static/js/2.035635a0.chunk.js"
+  },
+  {
+    "revision": "fb41e5c12c6d4c11161f",
+    "url": "/react-todo-list/static/js/runtime~main.fb41e5c1.js"
+  },
+  {
+    "revision": "ebfa7796357a93f9c44917a94367044d",
+    "url": "/react-todo-list/index.html"
+  }
+];
